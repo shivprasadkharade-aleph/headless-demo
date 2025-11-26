ab0679309eff17d06bf4ade623f493fa1b61d8c8
shivprasad <shivprasad.kharade@aleph-labs.com>
2025-11-27 03:00:32 +0800

    Bsic Hero Added back

---
+++ examples/kit-nextjs-skate-park/src/components/hero/BasicHero.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentParams, ComponentRendering } from '@sitecore-content-sdk/nextjs';
+
+interface BasicHeroProps {
+  rendering: ComponentRendering & { params: ComponentParams };
+  params: ComponentParams;
+}
+
+export const Default = (props: BasicHeroProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+
+  return (
+    <div className={`component ${props.params.styles}`} id={id ? id : undefined}>
+      <div className="component-content">
+        <p>BasicHero Component</p>
+      </div>
+    </div>
+  );
+};
